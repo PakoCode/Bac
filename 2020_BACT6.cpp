65ab0b7eb6a147d6581364f2a1c367033226a4d5
PakoCode <68905308+PakoCode@users.noreply.github.com>
2022-10-12 17:28:40 +0300

    Create 2020_BACT6.cpp

---
+++ 2020_BACT6.cpp
@@ -0,0 +1,10 @@
+SIII
+3.
+Algoritmul presupune o serie de pasi prin care
+aflam primele doua cifre, c1 si c2, c1 fiind un 
+numar care este divizor al lui p1, iar c2 este 
+p1/c1. La fel vom face si cu ultimele doua cifre,
+c3, c4, doar ca pt p2. C reprezinta defapt cele
+3 cifre din mijlocul numarului, care au fost
+obtinute parcurgand intervalu [0, 9] in ordine descrescatoare;
+
