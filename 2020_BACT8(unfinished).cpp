689baabd89c8d7ab25f42d4b1119cd3a95de3241
PakoCode <68905308+PakoCode@users.noreply.github.com>
2022-10-19 17:49:32 +0300

    Update 2020_BACT8(unfinished).cpp

--- 2020_BACT8(unfinished).cpp
+++ 2020_BACT8(unfinished).cpp
@@ -22,3 +22,7 @@ int main() {
     }
     return 0;
 }
+
+//Programul este eficient din punct de vedere al timpului de executie deoarece are o complexitate O(n) :)
+//Parcurgem fiecare element al sirului si contorizam frecventa ultimei sale cifre plus pozitia pe care apare ultima aparitie. Dupa asta, vom afla 
+//care este frecventa maxima a unei cifre si vom afisa pozitiile pe care se afla numerele precedate de un numar maxim de aparitii a ultimei sale cifre.
