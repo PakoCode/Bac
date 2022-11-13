a6048dc51d62fc673ba0e0ad7fbe1144fa144b3a
PakoCode <68905308+PakoCode@users.noreply.github.com>
2022-11-13 16:43:35 +0200

    Update 2020_BACT11(unfinished).cpp

--- 2020_BACT11(unfinished).cpp
+++ 2020_BACT11(unfinished).cpp
@@ -1,24 +1,15 @@
 s3 1 // unfinished;
 #include <iostream>
-
+#include <cmath>
 using namespace std;
-void patrate(int x, int y){
-    int s = 0, ok = 0;
-    for(int i = 1; i * i <= y; i ++){
-        
-        if(i * i > x){
-            if((i + 1) * (i + 1) <= y){
-            cout << i * i << '+';
-            ok = 1;
-            s = s + i * i;
-            }
-            else{
-            s = s + i * i;
-            cout << '=' << s;
-            
-                }
-        }
+void patrate(int x, int y)
+    int i = ceil(sqrt(x));
+    for(i; i < sqrt(y) - 1; i ++){
+        cout << i*i << '+';
+        s = s + i*i;
     }
+    s = s + i * i;
+    cout << i * i << '=' << s;
 }
 int main() {
     int x, y;
@@ -27,28 +18,28 @@ int main() {
     return 0;
 }
 
+2.
 #include <iostream>
 #include <string.h>
 using namespace std;
-
 int main() {
-    char s[101], *p, cuv[101], aux[101];
-    cin.getline(s, 101);
+    char s[101], *p, aux[101];
+    cin.getline(s,101);
     p = strtok(s, " ");
     while(p != NULL){
-        strcpy(cuv, p);
-        for(int i = 0; i < strlen(cuv); i ++){
-            if(cuv[0] >= "a" && cuv[0] <= "z"){
-                cuv[0] += 32;
+        char c[101];
+        strcpy(c, p);
+        for(int i = 0; i < strlen(c); i ++){
+            if(c[0] >'a' && c[0] < 'z') c[0] -= 32;
+            else if((i != 0) && (c[i] >'A' && c[i] < 'Z')){
+                c[i] += 32;
             }
-            else if(i =! 0 && cuv[i] >="A" && cuv[i] <="Z"){
-                cuv[i] -= 32;
-            }
-            strcat(cuv, aux);
-            strcat(aux, " ");
-            
         }
-        cout << aux;
+        strcat(aux, c);
+        strcat(aux, " ");
+        p = strtok(NULL, " ");
     }
+    strcpy(s, aux);
+    cout << s;
     return 0;
 }
