c29f1db6e1f2e4f7cf4ced7e94b946dc2200edca
PakoCode <68905308+PakoCode@users.noreply.github.com>
2022-11-13 15:39:42 +0200

    Create 2020_BACT12(unfinished).cpp

---
+++ 2020_BACT12(unfinished).cpp
@@ -0,0 +1,37 @@
+S1
+1.d;
+2.a;
+3.b;
+  f(54321) = f(5432)
+  f(5432) = f(543) 2
+  f(543) = f(54)
+  f(54) = f(5) 4
+  f(5) = f(0);
+4.b;
+5.c;
+
+S2
+1.
+a)2592
+  952;
+b)2000
+  2002
+  2022;
+d)
+  pentru c <- 9, 0, executa;
+  
+2.
+  struct flori{
+    int cod, nrVarietati, nrExemplare[10];
+  }f[20];
+
+3.
+  abcde
+  abcd6
+  1245 6
+  1345 6
+  2345 6;
+
+S3
+1.
+  
