4405a8772dce24f9bae1bcc91f97ffa50aeafac1
PakoCode <68905308+PakoCode@users.noreply.github.com>
2022-11-23 17:13:51 +0200

    Create 2023_Bac-Antrenament.cpp

---
+++ 2023_Bac-Antrenament.cpp
@@ -0,0 +1,23 @@
+S1
+1.b;
+2.d;
+  f(21,27) = 
+  24 f(22, 25) =
+  23 f(23, 23) = 
+  23 bun ii
+
+3.a;
+4.c;
+5.d;
+
+S2
+1.90;
+a) 4 3 11 25
+b) 10 15 2 {70, 74}
+d) 
+  x <- p;
+  cat timp x < q executa
+  blah blah
+  x ++;
+}
+scrie s
