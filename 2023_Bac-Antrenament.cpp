79e39dc48bb7400715f9309a578291be778c9c01
PakoCode <68905308+PakoCode@users.noreply.github.com>
2022-12-07 17:05:41 +0200

    Update 2023_Bac-Antrenament.cpp

--- 2023_Bac-Antrenament.cpp
+++ 2023_Bac-Antrenament.cpp
@@ -26,6 +26,30 @@ scrie s
 
 3.
 s1 = plantau fistic
-p = 8 - 
-  
-  
+p = 8;
+
+
+S3
+3.
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, x, min_par = 1000001, min_imp = 1000001, max_par = -1, max_imp = -1;
+    cin >> n;
+    for(int i = 1; i <= n; i ++){
+        cin >> x;
+        if(x % 2 == 0 && x < min_par) min_par = x;
+        else if (x % 2 != 0 && x < min_imp) min_imp = x;
+    }
+      for(int i = 1; i <= n; i ++){
+        cin >> x;
+        if(x % 2 == 0 && x > max_par) max_par = x;
+        else if (x % 2 != 0 && x > max_imp) max_imp = x;
+    }
+      if(min_par > max_imp && min_imp > max_par) cout << "da";
+      else cout << "nu";
+    return 0;
+};
+
